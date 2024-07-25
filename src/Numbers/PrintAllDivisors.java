87274720751726b3a9b90c5d975505e919e13ba5
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-25 06:50:52 +0530

    change

--- src/Numbers/PrintAllDivisors.java
+++ src/Numbers/PrintAllDivisors.java
@@ -1,4 +1,18 @@
 package Numbers;
 
 public class PrintAllDivisors {
+    public static void main (String[] args){
+        int n = 12; // Initialize 'n' with a value, for example, 12
+        printDivisors(n);
+
+
+    }
+
+    public static void  printDivisors(int N){
+        for (int i = 1; i <= N ; i++){
+            if (N % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
 }
