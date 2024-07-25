a038f211726c83500a562b600885e3829f56701c
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-26 03:39:21 +0530

    change

---
+++ src/Numbers/PrimeNumbers.java
@@ -0,0 +1,18 @@
+package Numbers;
+
+public class PrimeNumbers {
+    public static void main (String[] args){
+
+    }
+    static boolean isPrime(int N){
+        if (N <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
