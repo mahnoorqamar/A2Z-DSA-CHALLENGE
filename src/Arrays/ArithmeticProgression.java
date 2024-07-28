59aeda81372702c64cb3ffdf1de248e6136149eb
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-29 05:00:08 +0530

    Create ArithmeticProgression.java

---
+++ src/Arrays/ArithmeticProgression.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArithmeticProgression {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canMakeArithmeticProgression(int[] arr) {
+        return true;
+    }
+}
