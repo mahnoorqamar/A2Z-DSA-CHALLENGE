1377a2e1470cfcab6110a14e53c1b4f01e065010
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-30 06:22:00 +0530

    Update Hashmap.java

--- src/Hashmap/Hashmap.java
+++ src/Hashmap/Hashmap.java
@@ -1,6 +1,8 @@
 package Hashmap;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Hashmap {
     public static void main(String[] args) {
@@ -8,21 +10,32 @@ public class Hashmap {
         map.put("India", 120);
         map.put("USA", 130);
         map.put("China", 125);
-        System.out.println(map);
+//        System.out.println(map);
+//
+//        if (map.containsKey("Indonesia")) {
+//            System.out.println("present");
+//        } else {
+//            System.out.println("not present");
+//        }
+//
+//        System.out.println(map.get("China"));
+//
+//        Integer indonesiaValue = map.get("Indonesia");
+//        if (indonesiaValue != null) {
+//            System.out.println(indonesiaValue);
+//        } else {
+//            System.out.println("Key 'Indonesia' is not present in the map");
+//        }
 
-        if (map.containsKey("Indonesia")) {
-            System.out.println("present");
-        } else {
-            System.out.println("not present");
+        for (Map.Entry<String, Integer> e : map.entrySet()){
+            System.out.print(e.getKey() + " ");
+            System.out.print(e.getValue() + " ");
         }
 
-        System.out.println(map.get("China"));
-
-        Integer indonesiaValue = map.get("Indonesia");
-        if (indonesiaValue != null) {
-            System.out.println(indonesiaValue);
-        } else {
-            System.out.println("Key 'Indonesia' is not present in the map");
+        // just for keys
+        Set<String> keys = map.keySet();
+        for (String key : keys){
+            System.out.println(key);
         }
     }
 }
