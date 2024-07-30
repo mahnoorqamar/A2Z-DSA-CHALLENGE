92aaa9e2c4af5786a9a3b2c983f7f383979d5629
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-30 06:37:41 +0530

    Create ExampleLinkedList.java

---
+++ src/LinkedList/ExampleLinkedList.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+
+public class ExampleLinkedList {
+    class Node {
+        String data;
+        String next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        ExampleLinkedList list = new ExampleLinkedList();
+        
+    }
+}
