5a9b3feb8aaa27a683273c41eab6c7224d2eeb63
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-07-31 06:17:10 +0530

    day6

--- src/SortingTechniques/SelectingSort.java
+++ src/SortingTechniques/SelectingSort.java
@@ -2,29 +2,28 @@ package SortingTechniques;
 
 public class SelectingSort {
     public static void main(String[] args) {
-        int[] arr = {13, 46, 24, 52, 20, 9};
-        int n = arr.length; // Calculate the size of the array
-        selectionSort(arr, n);
 
-        // Print the sorted array
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the maximum item and swap with correct
+
+            int last = arr.length -i -1;
+            int maximum = getMaxIndex(arr, 0, last);
+
+            swap(arr, maximum, last);
         }
     }
 
-    public static void selectionSort(int[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) { // Outer loop for traversing the array
-            int min = i; // Initialize min to the current index i
-            for (int j = i + 1; j < n; j++) { // Start j from i + 1 to n
-                if (arr[j] < arr[min]) {
-                    min = j; // Update min if a smaller element is found
-                }
-            }
-            // Swap the found minimum element with the element at index i
-            if (min != i) {
-                swap(arr, i, min); // Pass indices to the swap function
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end ; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
             }
         }
+        return max;
     }
 
     public static void swap(int[] arr, int index1, int index2) {
