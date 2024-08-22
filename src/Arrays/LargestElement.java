5a5fc35745d8d1dd58d174edb67672e926ac7b24
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-08-23 00:48:35 +0530

    Update LargestElement.java

--- src/Arrays/LargestElement.java
+++ src/Arrays/LargestElement.java
@@ -3,7 +3,7 @@ package Arrays;
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = {4, 3, 5, 9, 5, 6, 10};
-        System.out.println(largest(arr));
+        System.out.println(Slargest(arr));
 
     }
 
@@ -16,5 +16,20 @@ public class LargestElement {
             }
         }
         return largest;
+    }
+
+    public static int Slargest(int[] arr){
+        int largest = arr[0];
+        int second = -1;
+
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] > largest){
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+        return second;
     }
 }
