7a9a8da0c1456d45209f06bb987b962ec169eec6
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-09-22 21:55:30 +0530

    bit

--- src/Numbers/PositionRightmost.java
+++ src/Numbers/PositionRightmost.java
@@ -1,4 +1,17 @@
 package Numbers;
 
 public class PositionRightmost {
+    public static int findRightMostSetBitPosition(int num) {
+        if (num == 0) return -1; // No set bit in zero
+
+        int position = 1; // Position is 1-based
+        int mask = num & -num; // Isolate the rightmost set bit
+
+        while ((mask >>= 1) != 0) {
+            position++;
+        }
+
+        return position;
+    }
+
 }
