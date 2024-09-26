92e3980bf4e4f8b4acda47167970e9a8ee834a05
mahnoor qamar <mahnoorqamar15@gmail.com>
2024-09-26 23:41:32 +0530

    concate

---
+++ src/String/StringConcataination.java
@@ -0,0 +1,13 @@
+package String;
+
+public class StringConcataination {
+    public static void main(String[] args) {
+        String s1 = "Mahnoor";
+        String s2 = "Minakshi";
+
+        System.out.println(concate(s1, s2));
+    }
+    public static String concate(String s1, String s2){
+        return s1 + s2;
+    }
+}
